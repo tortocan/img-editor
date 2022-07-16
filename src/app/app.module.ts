89800390e52652f9c13cc1034fce4418aca5020f
George Tortocan <george.tortocan@gmail.com>
2022-07-16 22:03:06 +0200

    Remove pkg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CanvasComponent } from './canvas/canvas.component';
-import { RulerModule } from '@ngx-canvas/ruler';
 
 @NgModule({
   declarations: [
@@ -15,8 +14,7 @@ import { RulerModule } from '@ngx-canvas/ruler';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    RulerModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
